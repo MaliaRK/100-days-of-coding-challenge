4d88b4f29c30e2eaaea38c15b03ad901f3cf128a
MaliaRK <160169166+MaliaRK@users.noreply.github.com>
2024-04-05 00:36:10 +0500

    100-days-of-coding-challenge

---
+++ 34-day.js
@@ -0,0 +1,11 @@
+// Answer 100:
+var squareRoot = Math.sqrt(144);
+console.log(squareRoot);
+// Answer 101:
+function randomNumber() {
+    return Math.floor(Math.random() * 10) + 1;
+}
+console.log(randomNumber());
+// Answer 102:
+var difference = Math.abs(-5 - 5);
+console.log(difference);
---
+++ 34-day.ts
@@ -0,0 +1,16 @@
+// Answer 100:
+const squareRoot: number = Math.sqrt(144);
+console.log(squareRoot);
+
+
+// Answer 101:
+function randomNumber(): number{
+    return Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(randomNumber());
+
+
+// Answer 102:
+const difference: number = Math.abs(-5 - 5);
+console.log(difference);
